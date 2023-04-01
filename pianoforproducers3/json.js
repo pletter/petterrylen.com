a5f9e390e70a51a66a5ed83c41edacda0a344918
pletter <petter.rylen@gmail.com>
2023-04-01 02:58:04 +0200

    new prod3

---
+++ pianoforproducers3/json.js
@@ -0,0 +1,345 @@
+let chapter1 = {
+  title: "Chapter 1",
+  description: "major and minor chords",
+  videos: [
+    { 
+      title: "The Major Circle",
+      description: "learn how the circle training works and practice your basic major chords",
+      videoId: "dKG8qUGK40o",
+    },
+    {
+      title: "Why the circle?",
+      description: "hej hej",
+      videoId: "PswVpeqSNYE",
+    },
+    {
+      title: "The minor circle",
+      description: "",
+      videoId: "PswVpeqSNYE",
+    },
+    {
+      title: "Two hand arpeggio",
+      description: "",
+      videoId: "xXsWyPiQLmk",
+    },
+    {
+      title: "Playalong 70 bpm",
+      description: "",
+      videoId: "YICiXC5FdAA",
+    },
+    {
+      title: "If you can't keep up",
+      description: "",
+      videoId: "48zeau9-reI",
+    },
+    {
+      title: "Playalong, chord by chord",
+      description: "",
+      videoId: "lhinvd9sPzM",
+    },
+    {
+      title: "Playalong 60 bpm",
+      description: "",
+      videoId: "_pgdyl0n3w0",
+    },
+    {
+      title: "The sustain pedal!",
+      description: "",
+      videoId: "MJEJR2G4VV0",
+    },
+    {
+      title: "Playalong 70 bpm",
+      description: "",
+      videoId: "sJC4QctF_DU",
+    },
+    {
+      title: "Say the names",
+      description: "",
+      videoId: "XE5zaHnRcaY",
+    },
+    {
+      title: "When to move on?",
+      description: "",
+      videoId: "8oB8g0RSP",
+    },
+    {
+      title: "Playalong 85 bpm",
+      description: "",
+      videoId: "",
+    },
+    {
+      title: "Keep your fingers down",
+      description: "",
+      videoId: "",
+    },
+    {
+      title: "Playalong 100 bpm",
+      description: "",
+      videoId: "",
+    },
+  ]
+}
+
+let chapter2 = {
+  title: "Chapter 2",
+  description: "learn the inversion ladder",
+  videos: [
+    {
+      title: "circle of figs",
+      description: "",
+      videoId: "dFYMOzoSDNE",
+    },
+    {
+      title: "circle of figs",
+      description: "",
+      videoId: "dFYMOzoSDNE",
+    },
+    {
+      title: "circle of figs",
+      description: "",
+      videoId: "dFYMOzoSDNE",
+    },
+    {
+      title: "circle of figs",
+      description: "",
+      videoId: "dFYMOzoSDNE",
+    },
+    {
+      title: "Major ladder, 3 times/chord",
+      description: "",
+      videoId: "TUVIl_2Bsh0",
+    },
+  ]
+}
+
+let chapter3 = {
+  title: "Chapter 3",
+  description: "let's add 9ths",
+  videos: [
+    {
+      title: "circle of pigs",
+      description: "",
+      videoId: "dFYMOzoSDNE",
+    },
+    {
+      title: "circle of wigs",
+      description: "",
+      videoId: "dFYMOzoSDNE",
+    },
+    {
+      title: "circle of trees",
+      description: "",
+      videoId: "dFYMOzoSDNE",
+    },
+    {
+      title: "circle of goodnight",
+      description: "",
+      videoId: "dFYMOzoSDNE",
+    },
+    {
+      title: "Major ladder, 3 times/chord",
+      description: "",
+      videoId: "TUVIl_2Bsh0",
+    },
+  ]
+}
+
+let allChapters = [ chapter1, chapter2, chapter3 ];
+
+
+
+let videoArray= [
+    {
+      "chapter": 1,
+      "video": "false",
+      "title": "CHAPTER ONE",
+      "text": "Lol text"
+    },
+    {
+      "chapter": 1,
+      "video": "dKG8qUGK40o",
+      "title": "1. The Major Circle",
+      "text": "learn how the circle training works and practice your basic major chords"
+    },
+    {
+      "chapter": 1,
+      "video": "PswVpeqSNYE",
+      "title": "2. Why the circle of fifths?",
+      "text": "sounds complicated, please explain?"
+    },
+    {
+      "chapter": 1,
+      "video": "Y-MIfPqlIdY",
+      "title": "3. The minor circle",
+      "text": "Let's see how the circle training works with minor chords"
+    },
+{
+      "chapter": 1,
+      "video": "xXsWyPiQLmk",
+      "title": "4. Two hand arpeggio",
+      "text": "Now we'll play with both hands and make an arpeggio"
+    },
+{
+      "chapter": 1,
+      "video": "YICiXC5FdAA",
+      "title": "5. Playalong 70 bpm",
+      "text": "Time to take our two hand arpeggio through the circle of fifths"
+    },
+{
+      "chapter": 1,
+      "video": "48zeau9-reI",
+      "title": "6. What if you can't keep up?",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "zkrUQRznB9w",
+      "title": "7. Playalong – Chord by chord",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "_pgdyl0n3w0",
+      "title": "8. Playalong 60 bpm",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "MJEJR2G4VV0",
+      "title": "9. The sustain pedal!",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "sJC4QctF_DU",
+      "title": "10. Playalong 70 bpm",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "XE5zaHnRcaY",
+      "title": "11. Say the names",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "8oB8g0RSP-w",
+      "title": "12. When to move on?",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "bYoocBTjEwo",
+      "title": "13. Playalong 85 bpm",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "8nMCtbw1kHE",
+      "title": "14. Keep your fingers down",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "gl_5qDvgowU",
+      "title": "15. Playalong 100bpm",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "MUXQt_F605k",
+      "title": "16. Playalong 120 bpm",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "G688v6lv1Rs",
+      "title": "17. Move on to the next chapter",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "D0oEvcnLQQo",
+      "title": "Bonus: How I write chords",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "3XaEmw1Sh1E",
+      "title": "Bonus: Playalong 120 bpm var. 1",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "Ehms5Wl-_KY",
+      "title": "Bonus: Playalong 120 bpm var. 2",
+      "text": ""
+    },
+{
+      "chapter": 1,
+      "video": "JcZBv9wUQG4",
+      "title": "Bonus: Playalong 140 bpm",
+      "text": ""
+    },
+    {
+      "chapter": 2,
+      "video": "false",
+      "title": "CHAPTER TWO",
+      "text": "Lol text"
+    },
+    {
+      "chapter": 2,
+      "video": "WTfsPz0Kq-M",
+      "title": "Video 1",
+      "text": "Lol text"
+    },
+    {
+      "chapter": 2,
+      "video": "LWJ__GpNNgY",
+      "title": "Video 2",
+      "text": "Lol text"
+    },
+    {
+      "chapter": 2,
+      "video": "WTfsPz0Kq-M",
+      "title": "Video 3",
+      "text": "Lol text"
+    },
+    {
+      "chapter": 2,
+      "video": "dFYMOzoSDNE",
+      "title": "Video 4",
+      "text": ""
+    },
+    {
+      "chapter": 3,
+      "video": "false",
+      "title": "CHAPTER THREE",
+      "text": "Let's add 9ths"
+    },
+    {
+      "chapter": 3,
+      "video": "WTfsPz0Kq-M",
+      "title": "Coming Soon",
+      "text": ""
+    },
+    {
+      "chapter": 3,
+      "video": "LWJ__GpNNgY",
+      "title": "Coming soon",
+      "text": ""
+    },
+    {
+      "chapter": 3,
+      "video": "WTfsPz0Kq-M",
+      "title": "Coming soon",
+      "text": ""
+    },
+    {
+      "chapter": 3,
+      "video": "dFYMOzoSDNE",
+      "title": "Coming soon",
+      "text": ""
+    }
+  ]
+  
