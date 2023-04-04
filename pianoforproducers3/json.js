7556ae5f8f4f6be0f7d8780e5c66dd1f0c590359
pletter <petter.rylen@gmail.com>
2023-04-04 14:03:42 +0200

    new vids

--- pianoforproducers3/json.js
+++ pianoforproducers3/json.js
@@ -125,7 +125,7 @@ let chapter1 = {
     {
       title: "140 bpm",
       description: "",
-      videoId: "",
+      videoId: "B1fq218z9HI",
       type: "2",
     },
 
@@ -139,93 +139,100 @@ let chapter2 = {
     {
       title: "Root, third, fifth",
       description: "",
-      videoId: "dFYMOzoSDNE",
+      videoId: "wWlTr13IKf8",
       type: "1",
     },
     {
       title: "What are inversions?",
       description: "",
-      videoId: "dFYMOzoSDNE",
+      videoId: "dN592OcxZnE",
       type: "1",
     },
     {
       title: "Why practice inversions?",
+      description: "pCrC159I_Ls",
+      videoId: "",
+      type: "1",
+
+    },
+    {
+      title: "The ladder exercise",
       description: "",
-      videoId: "dFYMOzoSDNE",
+      videoId: "kWCJElDg4_U",
       type: "1",
 
     },
     {
       title: "Playalong, slow major ladder",
       description: "",
-      videoId: "dFYMOzoSDNE",
+      videoId: "bjD-9LABnI8",
       type: "2",
 
     },
     {
       title: "Playalong, slow minor ladder",
       description: "",
-      videoId: "TUVIl_2Bsh0",
+      videoId: "cXZeL8t-lAU",
       type: "2",
     },
     {
       title: "Every single note",
       description: "",
-      videoId: "TUVIl_2Bsh0",
+      videoId: "IWc4MTbzo7I",
       type: "1",
     },
     {
       title: "Playalong 90 bpm",
       description: "",
-      videoId: "TUVIl_2Bsh0",
+      videoId: "SCBOyxxYwhQ",
       type: "2",
     },
     {
       title: "Playalong 110 bpm",
       description: "",
-      videoId: "TUVIl_2Bsh0",
+      videoId: "yro8A6UwjfA",
       type: "2",
     },
     {
       title: "If you can't keep up",
       description: "",
-      videoId: "TUVIl_2Bsh0",
+      videoId: "TdFjFMXjqQg",
       type: "1",
     },
     {
       title: "Playalong 130 bpm",
       description: "",
-      videoId: "TUVIl_2Bsh0",
+      videoId: "uydw0VPvDuA",
       type: "2",
     },
     {
       title: "Playalong 80 bpm x2",
       description: "",
-      videoId: "TUVIl_2Bsh0",
+      videoId: "w6Bp6yHHdbQ",
       type: "2",
     },
     {
-      title: "Move on if you want",
-      description: "",
-      videoId: "TUVIl_2Bsh0",
+      title: "Move on to next chapter",
+      description: "nMn104k4Hsc",
+      videoId: "",
       type: "1",
     },
     {
       title: "Playalong 100 bpm",
       description: "",
-      videoId: "TUVIl_2Bsh0",
+      videoId: "p8ztpsRu0B4",
       type: "2",
     },
     {
       title: "Playalong 120 bpm",
       description: "",
-      videoId: "TUVIl_2Bsh0",
+      videoId: "1EvYV13haj0",
       type: "2",
     },
     {
       title: "Playalong 140 bpm",
       description: "",
-      videoId: "TUVIl_2Bsh0",
+      videoId: "Z_4LHN9yqu0",
       type: "2",
     },
   ]
@@ -238,28 +245,28 @@ let chapter3 = {
     {
       title: "coming soon",
       description: "",
-      videoId: "dFYMOzoSDNE",
+      videoId: "",
       type: "2",
 
     },
     {
       title: "coming soon",
       description: "",
-      videoId: "dFYMOzoSDNE",
+      videoId: "",
       type: "2",
 
     },
     {
       title: "coming soon",
       description: "",
-      videoId: "dFYMOzoSDNE",
+      videoId: "",
       type: "2",
 
     },
     {
       title: "coming soon",
       description: "",
-      videoId: "dFYMOzoSDNE",
+      videoId: "",
       type: "2",
 
     },
@@ -280,36 +287,77 @@ let chapter4 = {
     {
       title: "The 3rd position circle",
       description: "",
-      videoId: "",
+      videoId: "aczT9IhxUpQ",
       type: "1",
     },
     {
       title: "3rd position, slowly",
       description: "",
-      videoId: "",
+      videoId: "DO-yQgP3V4w",
       type: "2",
     },
     {
       title: "The 5th position circle",
       description: "",
-      videoId: "",
-      type: "2",
+      videoId: "A6YlujTOs0Q",
+      type: "1",
     },
     {
       title: "5th position, slowly",
       description: "",
-      videoId: "",
+      videoId: "qZMZu_XQaZM",
       type: "2",
     },
     {
       title: "Both positions 90 bpm",
       description: "",
-      videoId: "",
+      videoId: "v5LdFN-D88Q",
       type: "2",
     },
   ]
 }
 
-let allChapters = [chapter1, chapter2, chapter3, chapter4,];
+let chapter5 = {
+  title: "Chapter 5",
+  description: "Sus4 (coming soon)",
+  videos: [
+    {
+      title: "coming soon",
+      description: "",
+      videoId: "",
+      type: "1",
+    },
+  ]
+}
+
+let chapter6 = {
+  title: "Chapter 6",
+  description: "7ths (coming soon)",
+  videos: [
+    {
+      title: "coming soon",
+      description: "",
+      videoId: "",
+      type: "1",
+    },
+  ]
+}
+
+let chapter7 = {
+  title: "Chapter 7",
+  description: "Inversion mastery (coming soon)",
+  videos: [
+    {
+      title: "coming soon",
+      description: "",
+      videoId: "",
+      type: "1",
+    },
+  ]
+}
+
+
+
+let allChapters = [chapter1, chapter2, chapter3, chapter4, chapter5, chapter6, chapter7];
 
 
